15e540f6689173505b477bb10631c7f97b483bc1
fmatallana <matallana@LAPTOP-L2UCPVVM.localdomain>
2025-12-12 12:21:05 -0500

    Mi primer archivo

---
+++ notas/comandos.py
@@ -0,0 +1,11 @@
+"""
+Repaso de comandos:
+
+pwd: Nos indica la ubicación actual en la consola.
+mkdir: Crear una nueva carpeta.
+ll: Visualizar el listado de archivos en la ubicación actual.
+clear: Limpiar la consola.
+git init: Inicializar un proyecto con Git.
+touch: Crear un nuevo archivo.
+python3 (nombre del archivo) hello.py para ejecutar el archivo desde la terminal 
+"""

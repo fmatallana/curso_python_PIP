d92ff89ac4d979b158c3481a38a1c8e529a04291
fmatallana <felipe10109@hotmail.com>
2025-12-13 13:33:40 -0500

    add new note

--- notas/comandos.py
+++ notas/comandos.py
@@ -8,4 +8,6 @@ clear: Limpiar la consola.
 git init: Inicializar un proyecto con Git.
 touch: Crear un nuevo archivo.
 python3 (nombre del archivo) hello.py para ejecutar el archivo desde la terminal 
+
+git add ../README.md (para subir un archivo especifico en el push)
 """
